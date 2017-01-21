42980d589a3014e0a0850367d738c4bc583d6e9a
Zloteanu Anastasia <plutoniumsoldier@gmail.com>
2017-01-21 09:08:43 +0000

    final changes

--- app/models/poll.js
+++ app/models/poll.js
@@ -3,9 +3,25 @@ var mongoose = require('mongoose');
 //poll schema
 var pollSchema = mongoose.Schema({
     name : String,
-    options : Array,
-    votes: Array,
+    options : [{}],
+    creator: String,
+    voters: Array,
     created: {type: Date, default:Date.now}
 });
+
+pollSchema.methods.update = function(){
+    var poll = this, s = 0;
+    this.options.forEach(function(option){
+        s += option.votes;
+    });
+    this.options.forEach(function(option){
+       option.perc = option.votes / s * 100; 
+    });
+    this.markModified('options');
+    this.save(function(err){
+        if(err)
+            console.log(err);
+    })
+}
 
 module.exports = mongoose.model('Poll', pollSchema);
--- app/routes.js
+++ app/routes.js
@@ -1,9 +1,14 @@
 //app routes
+var Poll = require('../app/models/poll');
 module.exports = function(app, passport) {
     
     //home page
     app.get('/', function(req, res){
-       res.render('index.ejs', { user: req.user}); 
+        Poll.find({}, function(err, polls) {
+            if(err)
+                throw err;
+            res.render('index.ejs', { user: req.user,polls: polls}); 
+        });
     });
     
     //login page
@@ -27,17 +32,16 @@ module.exports = function(app, passport) {
                                                                failureFlash: true}));
     //new poll
     app.get('/new', function(req, res){
-       /* if(!req.user){
-            res.redirect('/');
-        }*/
+        //redirect if there is no logged in user
+       if(!req.user){
+            req.flash('loginMessage', 'Log in to create a new poll!');
+            res.redirect('/login');
+        }
        res.render('new.ejs', {user:req.user}); 
     });
     
     //process poll form
     app.post('/new', function(req,res){
-        
-        //get poll schema
-        var Poll = require('../app/models/poll');
         
         //get poll name and options
         var name = req.body.name, options = req.body.options;
@@ -47,16 +51,119 @@ module.exports = function(app, passport) {
         //create a new poll with this data
         var newPoll = new Poll();
         newPoll.name = name;
-        newPoll.options = allOptions;
-        
+        newPoll.creator = req.user.local.username;
+        allOptions.forEach(function(option){
+           newPoll.options.push({option: option, votes: 0, perc: 0}); 
+        });
         //save poll
         newPoll.save(function(err) {
-            console.log("la");
             if (err)
                 throw err;
             return;
         });
-        res.send('Saved poll!');
+        res.redirect('/poll/' + newPoll._id);
+    });
+    //poll page
+    app.get('/poll/:id', function(req,res){
+       Poll.findById(req.params.id, function(err, poll){
+           if(err)
+             throw err;
+            var colors = ['progress-bar-success','progress-bar-info','progress-bar-warning','progress-bar-danger'];
+            res.render('poll.ejs', {poll: poll, user: req.user, message: req.flash('pollMessage'), colors:colors});
+       });
+    });
+    //get post votes
+    app.post('/poll/:id', function(req, res){
+        var user = req.user;
+        //get the poll
+       Poll.findById(req.params.id, function(err, poll){
+           if(err)
+             throw err;
+             //if there is no user logged in, or if the user already voted redirect
+            if( !user || poll.voters.indexOf(user.local.username) != -1){
+                var message = 'You need to be logged in!';
+                if (user)
+                    message = 'You already voted on this poll!';
+                req.flash('pollMessage', message);
+                res.redirect('/poll/' + req.params.id); 
+            }
+            else{
+                //check if custom option already exists
+                var found = false;
+                poll.options.forEach(function(option){
+                    if(option.option == req.body.custom)
+                        found = true;
+                });
+                if (found)
+                    req.body.sel = req.body.custom;
+                //if we have a custom option and it doesn't exists
+                if ( !found && req.body.custom){
+                    //push the new option & mark voter
+                    poll.options.push({option : req.body.custom, votes:1, perc: 0});
+                    poll.voters.push(user.local.username);
+                    poll.markModified('options');
+                    poll.markModified('voters');
+                    //save poll, update and redirect
+                    poll.save(function(err){
+                            if (err)
+                                throw err;
+                            //update poll percentages
+                            poll.update();
+                            res.redirect('/poll/'+req.params.id);
+                        });
+                } else{
+                //search for the option the user voted for
+                poll.options.forEach(function(option){
+                     if(option.option == req.body.sel){
+                         //add user vote and push user to list of users who voted
+                        option.votes ++;
+                        poll.voters.push(user.local.username);
+                        //mark changes
+                        poll.markModified('options');
+                        poll.markModified('voters');
+                        //save poll
+                        poll.save(function(err){
+                            if (err)
+                                throw err;
+                            //update poll percentages
+                            poll.update();
+                            res.redirect('/poll/'+req.params.id);
+                        });
+                    }
+                });
+            }
+            }
+       });
+    });
+    //mypolls
+    app.get('/myPolls',function(req, res){
+       if (!req.user){
+            req.flash('loginMessage', 'Login to see your polls.');
+            res.redirect('/login');
+       } else{
+        Poll.find({creator: req.user.local.username}, function(err,polls){
+           if(err)
+                throw err;
+            res.render('myPolls.ejs', {polls: polls});
+       })
+       }
+    });
+    app.get('/delete/:id', function(req, res){
+       var user = req.user, id = req.params.id;
+       Poll.findById(req.params.id, function(err, poll){
+           if(err)
+              throw err;
+            if(!user || poll.creator != user.local.username){
+                res.redirect('/');
+            }
+            else{
+                poll.remove(function(err){
+                    if(err)
+                    throw err;
+                    res.redirect('/myPolls');
+                });
+            }
+       })
     });
     //logout
     app.get('/logout', function(req, res){
